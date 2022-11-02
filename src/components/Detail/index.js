349c1f5d90d80efe464b7ff9042312b69096c6e2
ALAN-LAP <82869865+ALAN-LAP@users.noreply.github.com>
2022-11-02 15:25:52 +0700

    a

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -32,8 +32,16 @@ function Detail({ onClick, data }) {
                         <div className={style.price}>
                             {data.price}/{data.unit}
                         </div>
-                        <p className={style.label}>Quantity</p>
-                        <input type="number" name="number" min="1" max={data.amount} {...register('number', { required: true })} />
+                        <div className={style.action}>
+                            <div>
+                                <p className={style.label}>Remaining</p>
+                                <p>sl nè</p>
+                            </div>
+                            <div>
+                                <p className={style.label}>Quantity</p>
+                                <input type="number" name="number" min="1" max={data.amount} {...register('number', { required: true })} />
+                            </div>
+                        </div>
                     </div>
                     {Object.keys(errors).length !== 0 && (
                         <ul className={style.error}>
--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,9 @@
-import Header from './Header';
-import Sidebar from './Sidebar';
-import Footer from './Footer';
 import style from '~/components/Layout/DefaultLayout/DefaultLayout.module.scss'
 
 function DefaultLayout({ children }) {
     return (
         <div className={style.wrapper}>
             {children}
-
         </div>
 
         
