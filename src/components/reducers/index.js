52f5cba55f3ddb688eb3a36ec6eb7d65fb9facf1
ALAN <82869865+ALAN-LAP@users.noreply.github.com>
2022-11-12 11:28:51 +0700

    no action
    chưa làm gì hết

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,15 +1,3 @@
-<<<<<<< Updated upstream
-import { combineReducers } from 'redux';
-import Product from './product';
-import UserAdmin from './userAdmin';
-import login from './login';
-
-
-const allReducers = combineReducers({
-    _todoProduct: Product,
-    UserAdmin,
-    login,
-=======
 import { configureStore } from '@reduxjs/toolkit';
 import adminReducer from './admin';
 
@@ -17,5 +5,4 @@ export default configureStore({
     reducer: {
         admin: adminReducer,
     },
->>>>>>> Stashed changes
 });
