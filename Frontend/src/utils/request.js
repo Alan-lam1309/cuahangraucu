a95df05b2efbfd6044acd3f204ec05b56d242091
Lam Quoc Phu <d.fed26@gsoft01.tk>
2023-08-10 15:58:56 +0700

    test

--- Frontend/src/utils/request.js
+++ Frontend/src/utils/request.js
@@ -1,9 +1,8 @@
 import { ref, child, get, set, update } from 'firebase/database';
 import { database } from '~/firebase';
 
-
 const dbRef = ref(database);
-
+//phải sửa dùng axios
 export const getApi = async (option) => {
     const res = await get(child(dbRef, option));
     return res.val();
@@ -16,25 +15,3 @@ export const setApi = async (option, content = {}) => {
 export const updateApi = async (option, content = {}) => {
     update(child(dbRef, option), content);
 };
-
-
-
-
-
-
-// get(child(dbRef, `users`))
-//     .then((snapshot) => {
-//         if (snapshot.exists()) {
-//             console.log(snapshot.val());
-//         } else {
-//             console.log('No data available');
-//         }
-//     })
-//     .catch((error) => {
-//         console.error(error);
-//     });
-
-// // set(child(dbRef, `users/2`), {
-// //     id: 2,
-// //     name: 'aaa',
-// // });
